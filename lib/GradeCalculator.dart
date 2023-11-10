6193df933da432acc5bc4f006d6cb3674c9f4982
Tetrixstreams <122379072+Tetrixstreams@users.noreply.github.com>
2023-11-10 01:37:01 -0500

    Initial commit.

---
+++ lib/GradeCalculator.dart
@@ -0,0 +1,15 @@
+class GradeCalculator {
+  String letterGrade(int i) {
+    if (i >= 90) {
+      return 'A';
+    } else if (i >= 80) {
+      return 'B';
+    } else if (i >= 70) {
+      return 'C';
+    } else if (i >= 60) {
+      return 'D';
+    } else {
+      return 'F';
+    }
+  }
+}
