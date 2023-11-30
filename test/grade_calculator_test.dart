11c456fd18989025fe2644e7f6863d0dec93b348
Tetrixstreams <122379072+Tetrixstreams@users.noreply.github.com>
2023-11-30 18:11:22 -0500

    Rewrite to make failing tests
    Created tests that would fail on their own
    to follow steps of TDD.

---
+++ test/grade_calculator_test.dart
@@ -0,0 +1,36 @@
+import 'package:gradetool/grade_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final calculator = GradeCalculator();
+
+  // Define the common code outside of the individual test cases
+  void grade(int numericGrade, String expectedLetterGrade) {
+    final result = calculator.calculateGrade(numericGrade);
+    expect(result, expectedLetterGrade);
+  }
+
+  test('100 is an A', () {
+    grade(100, 'A');
+  });
+
+  test('86 is a B', () {
+    grade(86, 'B');
+  });
+
+  test('78 is a C', () {
+    grade(78, 'C');
+  });
+
+  test('65 is a D', () {
+    grade(65, 'D');
+  });
+
+  test('34 is an F', () {
+    grade(34, 'F');
+  });
+
+  test('0 is an F', () {
+    grade(0, 'F');
+  });
+}
